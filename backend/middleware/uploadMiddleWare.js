d3435addf2d8767c3da65241ac8d3d8bcef8abff
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-07-04 21:25:09 +0530

    "making-changes-to-middleware"

--- backend/middleware/uploadMiddleWare.js
+++ backend/middleware/uploadMiddleWare.js
@@ -1,17 +1,13 @@
 import multer from 'multer';
 import path from 'path';
-import fs from 'fs';
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadDir = path.resolve('test', 'data');
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => cb(null, file.originalname), 
+  destination: './uploads/', 
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));  
+  }
 });
 
-const upload = multer({ storage });
-export default upload;
+const uploadMiddleware = multer({ storage });
+
+export default uploadMiddleware;
