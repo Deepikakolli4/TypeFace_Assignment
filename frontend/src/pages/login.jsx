527a226eda8c18d1ac69119ff848da5b407e4bae
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-07-05 11:48:52 +0530

    "designing-login-register"

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,62 @@
+import React,{useState} from 'react'
+import { loginUser } from '../api/api'
+import { useNavigate } from 'react-router-dom'
+const login = () => {
+  const[formData , setFormData] = useState({
+    email : '',
+    password : ''
+  })
+  const [error,setError] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = async(e) =>{
+     e.preventDefault();
+     try{
+         const response = await loginUser(formData);
+         localStorage.setItem('token',response.data.token);
+         navigate('/dashboard');
+     }catch(err){
+        setError(err.response?.data?.messeage);
+     }
+  }
+  return (
+    <div className='form-container'>
+        <h2 className='page-title'>Login</h2>
+        {error && <p className='error-message'>{error}</p>}
+        <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+           <label htmlFor='email' className='form-label'>Email</label>
+           <input
+             type = "email"
+             id = "email"
+             name = "email"
+             value={formData.email}
+             onChange = {handlechange}
+             placeholder='Enter email'
+             className='form-input'
+             required
+           >
+           </input>
+        </div>
+         <div className='form-group'>
+           <label htmlFor='password' className='form-label'>Password</label>
+           <input
+             type = "password"
+             id = "password"
+             name = "password"
+             value={formData.password}
+             onChange = {handlechange}
+             placeholder='Enter password'
+             className='form-input'
+             required
+           >
+           </input>
+        </div>
+        <button type="submit" className='form-button'>
+            Login
+        </button>
+        </form>
+    </div>
+  )
+}
+
+export default login
---
+++ frontend/src/pages/register.jsx
@@ -0,0 +1,80 @@
+import React,{use, useState} from 'react'
+import { registerUser } from '../api/api'
+import { useNavigate } from 'react-router-dom'
+
+const register = () => {
+  const [formData,setFormData] = useState({
+    username : '',
+    email:'',
+    password:''
+  })
+  const [error,setError] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    try{
+        await registerUser(formData);
+        navigate('/login');
+    }catch(err){
+        setError(err.response?.data?.message);
+    }
+  }
+  const handleChange = async(e) =>{
+     setFormData({
+        ...formData,[e.target.name] : [e.target.value]
+     });
+  }
+  return (
+    <div className='form-container'>
+        <h2 className='page-title'>Register</h2>
+        {error && <p className='error-message'>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor='Username' className='form-label'>Username</label>
+                <input
+                  type = "text" 
+                  id = "username"
+                  name = "username"
+                  value = {formData.username}
+                  onChange={handleChange}
+                  placeholder='Enter username'
+                  className='form-input'
+                  required
+                >
+                </input>
+            </div>
+            <div className='form-group'>
+                <label htmlFor='email' className='form-label'>Email</label>
+                <input
+                  type = "text" 
+                  id = "email"
+                  name = "email"
+                  value = {formData.email}
+                  onChange={handleChange}
+                  placeholder='Enter email'
+                  className='form-input'
+                  required
+                >
+                </input>
+            </div>
+             <div className='form-group'>
+                <label htmlFor='password' className='form-label'>Password</label>
+                <input
+                  type = "text" 
+                  id = "password"
+                  name = "password"
+                  value = {formData.password}
+                  onChange={handleChange}
+                  placeholder='Enter password'
+                  className='form-input'
+                  required
+                >
+                </input>
+            </div>
+            <button type = "submit" className='form-button'>Register</button>
+        </form>
+    </div>
+  )
+}
+
+export default register
