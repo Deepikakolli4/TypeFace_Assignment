2f3fefd0faaafd9d519092dc6a45453325fc63ff
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-07-05 17:50:05 +0530

    "added-the-env"

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { urls } from '../constants/constants';
-//add to env
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
