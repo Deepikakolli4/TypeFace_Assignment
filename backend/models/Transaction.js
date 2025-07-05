ebb99831a171a0804bf6e85d19ee99ebd420d9f1
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-07-05 20:05:12 +0530

    "fixed-up"

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -11,7 +11,7 @@ const transactionSchema = new mongoose.Schema({
     },
     date : {
         type : Date,
-        require : true
+        required : true
     },
     category:{
          type : String,
